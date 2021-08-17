701798ad8e603600929c57f014fd77bb97b06721
Kevenho <cyclushk@gmail.com>
2021-08-16 22:06:58 -0300

    wip d3

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom'
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
 import { firebaseDatabase } from '../services/firebase';
@@ -21,7 +22,7 @@ type FirebaseQuestions = Record<string, {
   isHighlighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -40,7 +41,7 @@ export function Room() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([])
+  const [questions, setQuestions] = useState<QuestionType[]>([])
   const [title, setTitle] = useState('');
 
   const roomId = params.id;
@@ -128,7 +129,18 @@ export function Room() {
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
-
       </main>
+      <h4 className="question-list">
+      { questions.map((question) => {
+          return (
+            <Question
+            key={question.id}
+            content={question.content}
+            author={question.author} 
+            />
+          );
+        })}
+      </h4>
     </div>
-  )};
+  );
+}	
